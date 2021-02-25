cf1b7bea883bd13ec04b27b55a91f649207e0c91
Lorena Spiller <lorenaspiller@gmail.com>
2021-02-25 15:26:23 +0100

    fix prompt

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // richiesta nome
-var nome = prompt('Qual è il tuo nome?');
+var nome = prompt('Ciao! \nPronto a generare una password sicurissima? \nRispondi a queste domande! \nQual è il tuo nome?');
 document.getElementById('nome').innerHTML = nome;
 
 // richiesta cognome
-var cognome = prompt('Qual è il tuo cognome?');
+var cognome = prompt('Bene, ' + nome + ', qual è il tuo cognome?');
 document.getElementById('cognome').innerHTML = cognome;
 
 // richiesta colore preferito
-var colore = prompt('Qual è il tuo colore preferito?');
+var colore = prompt('Ottimo ' + nome + ' ' + cognome + '! Qual è il tuo colore preferito?');
 
 // generazione password
 document.getElementById('password').innerHTML = nome + cognome + colore + "27";
